b96b10a2b07e300151c1440485f51430a78d588c
Ilya7 <Ilya7@DESKTOP-EBFOKHG>
2019-08-07 14:25:46 +0300

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(1, 3, '*');         
+            p1.Draw();
+            
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            Console.ReadLine();
+        }    
+    }
+}
