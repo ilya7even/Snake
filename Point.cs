9ea0a15ec4feafe1085c917330c74d9262428215
Ilya7 <Ilya7@DESKTOP-EBFOKHG>
2019-08-15 02:45:54 +0300

    123

--- Point.cs
+++ Point.cs
@@ -48,5 +48,10 @@ namespace Snake
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
+        public void Clear()
+        {
+            sym = ' ';
+            Draw();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace Snake
 {
@@ -22,7 +23,19 @@ namespace Snake
             Point p = new Point(4, 5, '*');           
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+
             Console.ReadLine();
+            
+            
+
         }                 
     }
 }
